54bcbcbfec40a069f721429d8e2329cac40d082c
move <1223470574@qq.com>
2023-09-06 21:40:19 +0800

    'removelegacy'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
+// import legacy from '@vitejs/plugin-legacy'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -30,30 +30,31 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-      renderLegacyChunks: true,
-      polyfills: [
-        'es.symbol',
-        'es.promise',
-        'es.promise.finally',
-        'es/map',
-        'es/set',
-        'es.array.filter',
-        'es.array.for-each',
-        'es.array.flat-map',
-        'es.object.define-properties',
-        'es.object.define-property',
-        'es.object.get-own-property-descriptor',
-        'es.object.get-own-property-descriptors',
-        'es.object.keys',
-        'es.object.to-string',
-        'web.dom-collections.for-each',
-        'esnext.global-this',
-        'esnext.string.match-all'
-      ]
-    }),vue(),
+    // legacy({
+    //   targets: ['defaults', 'not IE 11'],
+    //   additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    //   renderLegacyChunks: true,
+    //   polyfills: [
+    //     'es.symbol',
+    //     'es.promise',
+    //     'es.promise.finally',
+    //     'es/map',
+    //     'es/set',
+    //     'es.array.filter',
+    //     'es.array.for-each',
+    //     'es.array.flat-map',
+    //     'es.object.define-properties',
+    //     'es.object.define-property',
+    //     'es.object.get-own-property-descriptor',
+    //     'es.object.get-own-property-descriptors',
+    //     'es.object.keys',
+    //     'es.object.to-string',
+    //     'web.dom-collections.for-each',
+    //     'esnext.global-this',
+    //     'esnext.string.match-all'
+    //   ]
+    // }),
+    vue()
   ],
 
 })
