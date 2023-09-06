6c2d6e9956ec0d37bbc72487473011b7322784be
move <1223470574@qq.com>
2023-09-06 17:16:43 +0800

    'jz-element-plus'

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import router from './router/index.js'
 import  axios from 'axios'
 import VueAxios from 'vue-axios'
-import 'element-plus/dist/index.css'
-import ElementPlus from 'element-plus'
+
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import logining from './App.vue'
 import { createStore } from 'vuex'
@@ -25,7 +24,7 @@ const app = createApp(App)
 
 
 app.use(VueAxios, axios)
-app.use(ElementPlus)
+
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
 
   resolve: {
@@ -21,6 +24,12 @@ export default defineConfig({
   // },
 
   plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     legacy({
       targets: ['defaults', 'not IE 11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
